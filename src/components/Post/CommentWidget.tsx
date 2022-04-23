0debe850977aa834787fce109c8954303333af2b
gjjang <haro220@naver.com>
2022-04-23 22:16:36 +0900

    first commit

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -2,7 +2,7 @@ import React, { createRef, FunctionComponent, useEffect } from 'react'
 import styled from '@emotion/styled'
 
 const src = 'https://utteranc.es/client.js'
-const repo = 'gjjang/gjjang.github.io' // 자신 계정의 레포지토리로 설정
+const repo = 'rlwnd0000/rlwnd0000.github.io' // 자신 계정의 레포지토리로 설정
 
 type UtterancesAttributesType = {
   src: string
