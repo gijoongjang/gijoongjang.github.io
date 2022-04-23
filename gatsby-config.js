a686ecc8a76f2b3c10885ba3fee9ca3acf9e4180
gjjang <haro220@naver.com>
2022-04-24 00:36:15 +0900

    MyBlog Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `개발 블로그`,
     description: `나의 개발 일지`,
     author: `GiJoongJang`,
-    siteUrl: 'https://rlwnd0000.github.io/',
+    siteUrl: 'https://gijoongjang.github.io/',
   },
   plugins: [
     {
@@ -85,7 +85,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://rlwnd0000.github.io/',
+        siteUrl: 'https://gijoongjang.github.io/',
         stripQueryString: true,
       },
     },
