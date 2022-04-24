47b26155332dc16e729f65bf805ea3a19ab2d330
gjjang <haro220@naver.com>
2022-04-24 12:49:57 +0900

    Utterances 댓글 추가

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,56 +1,57 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import { PostPageItemType } from 'types/PostItem.types' 
+import { PostPageItemType } from 'types/PostItem.types'
 import Template from 'components/Common/Template'
 import PostHead from 'components/Post/PostHead'
 import PostContent from 'components/Post/PostContent'
+import CommentWidget from 'components/Post/CommentWidget'
 
 type PostTemplateProps = {
-    data: {
-      allMarkdownRemark: {
-        edges: PostPageItemType[]
-      }
-    }
-    location: {
-      href: string
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[]
     }
   }
+  location: {
+    href: string
+  }
+}
 
-  const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
-    data: {
-      allMarkdownRemark: { edges },
-    },
-    location: { href },
-  }) {
-    const {
-      node: {
-        html,
-        frontmatter: {
-          title,
-          summary,
-          date,
-          categories,
-          thumbnail: {
-            childImageSharp: { gatsbyImageData },
-            publicURL,
-          },
+const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+  location: { href },
+}) {
+  const {
+    node: {
+      html,
+      frontmatter: {
+        title,
+        summary,
+        date,
+        categories,
+        thumbnail: {
+          childImageSharp: { gatsbyImageData },
+          publicURL,
         },
       },
-    } = edges[0];
-  
-    return (
-      <Template title={title} description={summary} url={href} image={publicURL}>
-        <PostHead
-          title={title}
-          date={date}
-          categories={categories}
-          thumbnail={gatsbyImageData}
-        />
-        <PostContent html={html} />
-      </Template>
-    )
-  }
+    },
+  } = edges[0]
 
+  return (
+    <Template title={title} description={summary} url={href} image={publicURL}>
+      <PostHead
+        title={title}
+        date={date}
+        categories={categories}
+        thumbnail={gatsbyImageData}
+      />
+      <PostContent html={html} />
+      <CommentWidget />
+    </Template>
+  )
+}
 export default PostTemplate
 
 export const queryMarkdownDataBySlug = graphql`
